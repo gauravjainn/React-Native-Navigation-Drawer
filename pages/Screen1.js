598575bbecf99129e6e799b775519cdb2ff13f49
gaurav.jain <itsgauravjain.gj@gmail.com>
2020-03-12 16:13:02 +0530

    added map design

--- pages/Screen1.js
+++ pages/Screen1.js
@@ -4,19 +4,31 @@ import {
   StyleSheet,
   View,
   Text,
+  FlatList,
+  ActivityIndicator,
   TextInput,
   TouchableOpacity,
 } from 'react-native';
 import SwipeablePanel from 'rn-swipeable-panel';
-import {SocialIcon} from 'react-native-elements';
+import {SocialIcon, List, ListItem} from 'react-native-elements';
+var _ = require('lodash');
 
 export default class Screen1 extends Component {
   constructor(props) {
     super(props);
     this.state = {
       swipeablePanelActive: false,
+      // loading: false,
+      // data: [],
+      // pageToken: '',
+      // refreshing: false,
+      // siteTitle: '',
     };
   }
+
+  // static navigationOptions = {
+  //   title: 'Restaurant List',
+  // };
 
   componentDidMount = () => {
     this.openPanel();
@@ -28,10 +40,92 @@ export default class Screen1 extends Component {
 
   closePanel = () => {
     this.setState({swipeablePanelActive: false});
-    // setTimeout(() => {
-    //   this.openPanel();
-    // }, 1000);
   };
+
+  // fetchData = () => {
+  //   const {pageToken} = this.state;
+  //   const urlFirst = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=24.5854,73.7125&radius=500&type=restaurant&key=AIzaSyAAQ1Cppz62lgwYEJjzrkty7Nzi5ZYNCSM`;
+  //   const urlNext = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=24.5854,73.7125&radius=500&type=restaurant&key=AIzaSyAAQ1Cppz62lgwYEJjzrkty7Nzi5ZYNCSM&pagetoken=${pageToken}`;
+
+  //   let url = pageToken === '' ? urlFirst : urlNext;
+  //   console.log(url);
+  //   console.log('url');
+  //   this.setState({loading: true});
+  //   fetch(url)
+  //     .then(res => {
+  //       return res.json();
+  //     })
+  //     .then(res => {
+  //       const arrayData = _.uniqBy([...this.state.data, ...res.results], 'id');
+
+  //       this.setState({
+  //         siteTitle: 'Resturants Near By',
+  //         data: pageToken === '' ? res.results : arrayData,
+  //         loading: false,
+  //         refreshing: false,
+  //         pageToken: res.next_page_token,
+  //       });
+  //     })
+  //     .catch(error => {
+  //       console.log(error);
+  //       this.setState({loading: false});
+  //     });
+  // };
+  // renderSeparator = () => {
+  //   return (
+  //     <View
+  //       style={{
+  //         height: 1,
+  //         width: '86%',
+  //         backgroundColor: '#CED0CE',
+  //         marginLeft: '14%',
+  //       }}
+  //     />
+  //   );
+  // };
+  // renderHeader = () => {
+  //   return (
+  //     <Text
+  //       style={{
+  //         alignSelf: 'center',
+  //         fontWeight: 'bold',
+  //         fontSize: 20,
+  //         marginBottom: 10,
+  //       }}>
+  //       {this.state.siteTitle}
+  //     </Text>
+  //   );
+  // };
+  // renderFooter = () => {
+  //   if (this.state.pageToken === undefined) return null;
+
+  //   return (
+  //     <View
+  //       style={{
+  //         paddingVertical: 20,
+  //         borderTopWidth: 1,
+  //         borderColor: '#CED0CE',
+  //       }}>
+  //       <ActivityIndicator animating size="large" />
+  //     </View>
+  //   );
+  // };
+
+  // handleRefresh = () => {
+  //   this.setState(
+  //     {
+  //       pageToken: '',
+  //       refreshing: true,
+  //     },
+  //     () => {
+  //       this.fetchData();
+  //     },
+  //   );
+  // };
+
+  // handleLoadMore = () => {
+  //   this.fetchData();
+  // };
 
   render() {
     return (
@@ -51,7 +145,7 @@ export default class Screen1 extends Component {
             onPress={() => {
               //Action to perform on press of Social Icon
               this.openPanel();
-             // alert('twitter');
+              // alert('twitter');
             }}
           />
         </View>
@@ -75,9 +169,20 @@ export default class Screen1 extends Component {
                 <SocialIcon
                   //Social Icon using react-native-elements
                   type="twitter"
+                  onPress={() => {
+                    //Action to perform on press of Social Icon
+                  //  this.fetchData();
+                    // alert('twitter');
+                  }}
                   //Type of Social Icon
                 />
-                <SocialIcon type="gitlab" />
+                <SocialIcon type="gitlab" 
+                  onPress={() => {
+                    //Action to perform on press of Social Icon
+                   // this.fetchData();
+                    // alert('twitter');
+                  }}
+                  />
                 <SocialIcon type="medium" />
                 <SocialIcon type="facebook" />
                 <SocialIcon type="instagram" />
@@ -92,6 +197,41 @@ export default class Screen1 extends Component {
             </View>
           </View>
         </SwipeablePanel>
+       {/* <View style={styles.ListContainer}>    
+        <FlatList
+          data={this.state.data}
+          keyExtractor={item => item.id}
+          ListHeaderComponent={this.renderHeader}
+          ListFooterComponent={this.renderFooter}
+          renderItem={({item}) => {
+            const rating = item.rating ? item.rating : 'na';
+
+            return (
+              <View>
+                <ListItem
+                  roundAvatar
+                  title={`${item.name}` + ' (' + `${rating}` + ')'}
+                  subtitle={`${item.vicinity}`}
+                  avatar={{uri: item.icon}}
+                  containerStyle={{borderBottomWidth: 0}}
+                />
+                <View
+                  style={{
+                    height: 1,
+                    width: '86%',
+                    backgroundColor: '#CED0CE',
+                    marginLeft: '14%',
+                  }}
+                />
+              </View>
+            );
+          }}
+          onRefresh={this.handleRefresh}
+          refreshing={this.state.refreshing}
+          onEndReached={this.handleLoadMore}
+          onEndReachedThreshold={50}
+        />
+      </View> */}
       </View>
     );
   }
@@ -126,5 +266,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#71C488',
     borderRadius: 20,
     alignItems: 'center',
+  },
+   ListContainer: {
+    height: '70%',
   },
 });
--- pages/Screen2.js
+++ pages/Screen2.js
@@ -1,26 +1,150 @@
 //This is an example code for NavigationDrawer//
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 //import react in our code.
-import { StyleSheet, View, Text } from 'react-native';
-// import all basic components
+import {StyleSheet, View, Text, Dimensions, TextInput, TouchableOpacity} from 'react-native';
+import MapView from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 
-export default class Screen2 extends Component {
+
+const {width, height} = Dimensions.get('window');
+const ASPECT_RATIO = width / height;
+const LATITUDE = 26.9124;
+const LONGITUDE = 75.7873;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+const GOOGLE_MAPS_APIKEY = 'AIzaSyAAQ1Cppz62lgwYEJjzrkty7Nzi5ZYNCSM';
+
+// import all basic components
+class Screen2 extends Component {
   //Screen2 Component
+  constructor(props) {
+    super(props);
+
+    // AirBnB's Office, and Apple Park
+    this.state = {
+      coordinates: [
+        {
+          latitude: 26.9124,
+          longitude: 75.7873,
+        },
+        {
+          latitude: 24.5854,
+          longitude:73.7125,
+        },
+      ],
+    };
+
+    this.mapView = null;
+  }
+
+  // onMapPress = e => {
+  //   this.setState({
+  //     coordinates: [...this.state.coordinates, e.nativeEvent.coordinate],
+  //   });
+  // };
+
   render() {
+       var mapStyle=[{"elementType": "geometry", "stylers": [{"color": "#242f3e"}]},{"elementType": "labels.text.fill","stylers": [{"color": "#746855"}]},{"elementType": "labels.text.stroke","stylers": [{"color": "#242f3e"}]},{"featureType": "administrative.locality","elementType": "labels.text.fill","stylers": [{"color": "#d59563"}]},{"featureType": "poi","elementType": "labels.text.fill","stylers": [{"color": "#d59563"}]},{"featureType": "poi.park","elementType": "geometry","stylers": [{"color": "#263c3f"}]},{"featureType": "poi.park","elementType": "labels.text.fill","stylers": [{"color": "#6b9a76"}]},{"featureType": "road","elementType": "geometry","stylers": [{"color": "#38414e"}]},{"featureType": "road","elementType": "geometry.stroke","stylers": [{"color": "#212a37"}]},{"featureType": "road","elementType": "labels.text.fill","stylers": [{"color": "#9ca5b3"}]},{"featureType": "road.highway","elementType": "geometry","stylers": [{"color": "#746855"}]},{"featureType": "road.highway","elementType": "geometry.stroke","stylers": [{"color": "#1f2835"}]},{"featureType": "road.highway","elementType": "labels.text.fill","stylers": [{"color": "#f3d19c"}]},{"featureType": "transit","elementType": "geometry","stylers": [{"color": "#2f3948"}]},{"featureType": "transit.station","elementType": "labels.text.fill","stylers": [{"color": "#d59563"}]},{"featureType": "water","elementType": "geometry","stylers": [{"color": "#17263c"}]},{"featureType": "water","elementType": "labels.text.fill","stylers": [{"color": "#515c6d"}]},{"featureType": "water","elementType": "labels.text.stroke","stylers": [{"color": "#17263c"}]}];
     return (
-      <View style={styles.MainContainer}>
-        <Text style={{ fontSize: 23 }}> Screen 2 </Text>
-      </View>
+        <View style={styles.container}>
+
+      <MapView
+        initialRegion={{
+          latitude: LATITUDE,
+          longitude: LONGITUDE,
+          latitudeDelta: LATITUDE_DELTA,
+          longitudeDelta: LONGITUDE_DELTA,
+        }}
+         customMapStyle={mapStyle}
+        style={StyleSheet.absoluteFill}
+        ref={c => (this.mapView = c)}
+        // onPress={this.onMapPress}
+        >
+        {this.state.coordinates.map((coordinate, index) => (
+          <MapView.Marker key={`coordinate_${index}`} coordinate={coordinate} />
+        ))}
+        {this.state.coordinates.length >= 2 && (
+          <MapViewDirections
+            origin={this.state.coordinates[0]}
+            waypoints={
+              this.state.coordinates.length > 2
+                ? this.state.coordinates.slice(1, -1)
+                : null
+            }
+            destination={
+              this.state.coordinates[this.state.coordinates.length - 1]
+            }
+            apikey={GOOGLE_MAPS_APIKEY}
+            strokeWidth={3}
+            strokeColor="hotpink"
+            optimizeWaypoints={true}
+            onStart={params => {
+              console.log(
+                `Started routing between "${params.origin}" and "${params.destination}"`,
+              );
+            }}
+            onReady={result => {
+              console.log(`Distance: ${result.distance} km`);
+              console.log(`Duration: ${result.duration} min.`);
+
+              this.mapView.fitToCoordinates(result.coordinates, {
+                edgePadding: {
+                  right: width / 20,
+                  bottom: height / 20,
+                  left: width / 20,
+                  top: height / 20,
+                },
+              });
+            }}
+            onError={errorMessage => {
+              // console.log('GOT AN ERROR');
+            }}
+          />
+        )}
+
+      </MapView>
+
+
+      <TouchableOpacity
+              style={styles.SubmitButtonStyle}
+              activeOpacity={.5}
+              onPress={this.CheckTextInput} >
+
+              <Text style={styles.TextStyle}> click </Text>
+            </TouchableOpacity>
+  </View>
+      
     );
   }
 }
 
 const styles = StyleSheet.create({
-  MainContainer: {
+   container: {
     flex: 1,
-    paddingTop: 20,
-    alignItems: 'center',
-    marginTop: 50,
     justifyContent: 'center',
+    alignItems: 'center'
   },
+ input: {
+    color: '#7f8ec5',
+    width: 300,
+    height: 50,
+    borderWidth: 0,
+    borderColor: 'black',
+    marginBottom: 10,
+    borderRadius: 20,
+    backgroundColor: '#cccccc',
+  },
+   SubmitButtonStyle: {
+    marginTop: 20,
+    width: 300,
+    height: 40,
+    padding: 10,
+    backgroundColor: '#71C488',
+    borderRadius: 20,
+    alignItems: 'center'
+  },
+  
 });
+  
+export default Screen2;
